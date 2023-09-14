1a6bd8545bb5e516ea89d45bc4cf17b60261119b
desarrolloface <desarrolloface@ubiobio.cl>
2023-09-13 22:08:30 -0300

    url_connect_backend

--- env.js
+++ env.js
@@ -1,8 +1,6 @@
-/* const URL_CONNECT_BACKEND = 'http://192.168.1.145:3002' */
-/* const URL_CONNECT_BACKEND = 'http://146.83.194.142:1401' */ 
-/* const URL_CONNECT_BACKEND = "http://172.20.10.3:3002"; */
-const URL_CONNECT_BACKEND = 'http://146.83.194.142:1262'
+const URL_CONNECT_BACKEND =
+  /* 'http://Servidor:Puerto_Apache' */
 
-module.exports = {
-  URL_CONNECT_BACKEND,
-};
+  (module.exports = {
+    URL_CONNECT_BACKEND,
+  });
